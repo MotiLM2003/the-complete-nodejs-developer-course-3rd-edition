43cd6e2c2acbbaacb090bc9f070e9f4c15718a27
Moti Elmakies <motiphone2003@gmail.com>
2022-01-12 02:16:42 +0200

    handling argv/yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,52 @@
 // const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 
 const getNotes = require('./notes.js');
-// console.log(getNotes('note 1'));
 
-// console.log(validator.isURL('https://mailcom'));
+// customize yargs version
+// yargs.version('1.1.0');
 
-const log = console.log;
-log(chalk.blue.inverse('hello', 'world'));
+// create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'note body',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: (argv) => {
+    console.log(`Title: ${argv.title}, Body: ${argv.body}`);
+  },
+});
 
-console.log(chalk.green('Success'));
+// create  remove command
+yargs.command({
+  command: 'remove',
+
+  describe: 'Removing a note',
+  handler: () => console.log('Removing an old note.'),
+});
+
+// read
+yargs.command({
+  command: 'list',
+  describe: 'get a list of notes',
+  handler: () => console.log('showing list of notes'),
+});
+
+yargs.command({
+  command: 'read',
+  describe: 'reading a note',
+  handler: () => console.log('reading a specific note'),
+});
+
+yargs.parse();
