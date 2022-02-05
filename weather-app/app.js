832c7fef9a83acbb936121547166936914c3a41a
Moti Elmakies <motiphone2003@gmail.com>
2022-02-05 23:54:26 +0200

    callback Abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,7 @@
 const request = require('request');
 
 const axios = require('axios');
+const { config } = require('process');
 
 // const data = require('./test.json');
 
@@ -12,27 +13,51 @@ const url =
 const geoURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${geoAccess}&limit=1`;
 // const url = 'test.json';
 
-request({ url, json: true }, (err, response) => {
-  if (err) {
-    console.log('unable to connect to weeather service!');
-  } else if (response.body.error) {
-    console.log('unable to find location');
-  } else {
-    const data = response.body;
-    const feelslike = data.current.feelslike;
-    const temp = data.current.temperature;
-    console.log(
-      `${data.current.weather_descriptions[0]}. it's currently : ${temp} degress out. it feels like ${feelslike} degress out.`
-    );
-  }
-});
-
-// const feelslike = data.current.feelslike;
-// const temp = data.current.temperature;
+// request({ url, json: true }, (err, response) => {
+//   if (err) {
+//     console.log('unable to connect to weeather service!');
+//   } else if (response.body.error) {
+//     console.log('unable to find location');
+//   } else {
+//     const data = response.body;
+//     const feelslike = data.current.feelslike;
+//     const temp = data.current.temperature;
+//     console.log(
+//       `${data.current.weather_descriptions[0]}. it's currently : ${temp} degress out. it feels like ${feelslike} degress out.`
+//     );
+//   }
+// });
 
 // request({ url: geoURL, json: true }, (err, response) => {
-//   const data = response.body;
-//   const latitude = data.features[0].center[1];
-//   const longitude = data.features[0].center[0];
-//   console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+//   const { message, features } = response.body;
+//   if (err) {
+//     console.log('unable to access to location services.');
+//   } else if (message) {
+//     console.log(`Error: ${message}`);
+//   } else if (features.length === 0) {
+//     console.log('No locations found.');
+//   } else {
+//     const latitude = features[0].center[1];
+//     const longitude = features[0].center[0];
+//     console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+//   }
 // });
+
+const geoCode = (address, callback) => {
+  const geoURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+    address
+  )}.json?access_token=${geoAccess}&limit=1`;
+
+  request({ url: geoURL, json: true }, (err, response) => {
+    const { message, features } = response.body;
+    if (err) {
+      callback({ error: 'unable to access to location services.' });
+    } else if (message) {
+      callback({ error: message });
+    } else if (features.length === 0) {
+      callback({ error: 'No locations found.' });
+    }
+  });
+};
+
+geoCode('Tel Aviv', (err, data) => {});
