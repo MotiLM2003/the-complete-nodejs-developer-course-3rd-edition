23439ba202af310c39496cac52f03ccf52bc5cd5
Moti Elmakies <motiphone2003@gmail.com>
2022-02-11 21:22:14 +0200

    handle bars (Daynamic pages) - 01

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,18 +2,28 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
-
 const app = express();
-console.log('-', publicPath, '-');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicPath));
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>main page</h1>');
+  res.render('index', {
+    title: 'Dynamic',
+    name: 'Moti Elmakies',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About page', name: 'Moti Elmakies' });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', { title: 'Help page', name: 'Moti Elmakies' });
 });
 app.get('/weather', (req, res) => {
-  res.send('weather page');
+  res.send({ location: 'Bat Yam', forecast: 'Its hot as hell in here' });
 });
 
 app.listen(PORT, () => {
