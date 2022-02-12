5195330687b0edd62bb9e7cbdabacc5e57a66b5e
Moti Elmakies <motiphone2003@gmail.com>
2022-02-12 13:39:55 +0200

    completed setting user form

--- web-server/public/scripts/js.js
+++ web-server/public/scripts/js.js
@@ -1,11 +1,22 @@
 console.log('loading local javascript file');
 
-fetch('http://localhost:5000/weather?address=!').then((res) => {
-  res.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data);
-    }
-  });
+const myform = document.querySelector('form');
+
+myform.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const search = document.querySelector('#search').value;
+  alert(search);
+  getForecast(search);
 });
+
+const getForecast = (address) => {
+  fetch(`http://localhost:5000/weather?address=${address}`).then((res) => {
+    res.json().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data);
+      }
+    });
+  });
+};
