10aba25cce26bd8f12177a64dfab627d979bfcad
Moti Elmakies <motiphone2003@gmail.com>
2022-02-22 13:44:28 +0200

    adding share location to chat app

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,10 +1,50 @@
 const socket = io();
-// const btnIncButton = document.getElementById('btnIncButton');
-// const currentCount = document.getElementById('currentCount');
-// btnIncButton.addEventListener('click', () => {
-//   socket.emit('inc');
-// });
-// socket.on('countUpdate', (count) => {
-//   currentCount.textContent = count;
-//   console.log('the count has been update', count);
-// });
+
+const txtMessage = document.getElementById('txtMessage');
+const btnSendMessage = document.getElementById('btnSendMessage');
+const btnShareLocation = document.getElementById('btnShareLocation');
+let messages = '';
+txtMessage.value = '';
+
+txtMessage.addEventListener('keyup', (e) => {
+  console.log(e.key);
+  if (e.key === '13' || e.key === 'Enter') {
+    sendUserMessage();
+  }
+});
+btnSendMessage.addEventListener('click', () => {
+  sendUserMessage();
+});
+
+btnShareLocation.addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('You dont have support for geolocation');
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const { latitude, longitude } = position.coords;
+    socket.emit('shareLocation', { latitude, longitude });
+  });
+});
+const sendUserMessage = () => {
+  let message = txtMessage.value;
+  if (!message || !(message.length > 0)) return;
+  socket.emit('sendMessage', message);
+  txtMessage.value = '';
+  txtMessage.focus();
+};
+
+socket.on('newUserMessage', (message) => {
+  console.log(message);
+});
+
+socket.on('userLeft', (message) => {
+  console.log('user left the building');
+});
+
+socket.on('messageRecived', (message) => {
+  console.log('new message', message);
+  messages += `<br /> ${message}`;
+  document.getElementById('messages').innerHTML = messages;
+  //   txtMessage.value = '';
+});
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -11,17 +11,21 @@ app.use(express.static(path.join(__dirname, '../public/')));
 
 let count = 0;
 io.on('connection', (socket) => {
-  console.log('new connection');
   // single welcome user to the connected client
-  socket.emit('welcome', count);
+  socket.emit('newUserMessage', 'Welcome to the chat!');
+  socket.broadcast.emit('newUserMessage', 'new user has joined the chat!');
+  socket.on('sendMessage', (message) => {
+    io.emit('messageRecived', message);
+  });
 
-  // socket.on('inc', () => {
-  //   count++;
-  //   // socket.emit('countUpdate', count);
-  //   io.emit('countUpdate', count);
-  // });
+  socket.on('shareLocation', (coords) => {
+    console.log(coords);
+    io.emit('messageRecived', JSON.stringify(coords));
+  });
+  socket.on('disconnect', () => {
+    io.emit('userLeft', 'user has left');
+  });
 });
-
 server.listen(3000, () => {
   console.log('listining');
 });
